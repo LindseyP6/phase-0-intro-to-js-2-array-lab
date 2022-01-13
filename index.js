25ac82fe79c305fae4e7aee0c6e8163cce038dcb
Lindsey <lindsey.pregent@gmail.com>
2022-01-13 11:35:02 -0500

    commit tree fa611ec8668424f8bb5fdf8b0ce02b1eeca9c829

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(newName) {
+    let newCats = [...cats, newName]
+    return newCats;
+}
+
+function prependCat(newName) {
+    let newCats = [newName, ...cats]
+    return newCats;
+}
+
+//new array
+function removeLastCat() {
+    return cats.slice(1);
+}
+
+// new array
+function removeFirstCat() {
+    return cats.slice(0, cats.length - 1);
+}
